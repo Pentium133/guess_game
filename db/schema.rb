1646cb25fa2beec509d225d740671799806de4b0
Serg Puhoff <github@puhoff.com>
2015-04-30 00:04:38 +0300

    add season

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429205648) do
+ActiveRecord::Schema.define(version: 20150429210258) do
+
+  create_table "seasons", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
