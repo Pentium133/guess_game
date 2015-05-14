2083aec1c2f8f11636e176a3ea9f2141b393cefd
Serg Puhoff <github@puhoff.com>
2015-05-14 11:48:27 +0300

    stage type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
   def current_page_tag_if(options)
     '<span class="current-page"></span>'.html_safe if params.merge(options) == params
   end
+
+  def stage_type(stage)
+    case stage.stage_type
+      when 'itt'
+        return '<span class="badge badge-info">ITT</span>'.html_safe
+      when 'ttt'
+        return '<span class="badge badge-info">TTT</span>'.html_safe
+      else
+        return ''
+    end
+  end
 end
