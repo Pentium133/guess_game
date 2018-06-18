6c16b3c0529bac15fd3728ac1fdc79e69d833607
Serg <serg@fast-report.com>
2018-06-19 00:03:34 +0300

    показ очков

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,6 +2,7 @@ class RoundsController < ApplicationController
   def show
     @round = Round.friendly.find(params[:id])
     @page_title = @round.name
+    @overall = @round.get_overall
 
     add_breadcrumb I18n.t('menu.tournaments'), :tournaments_path
     add_breadcrumb @round.tournament.name, tournament_path(@round.tournament)
--- app/models/round.rb
+++ app/models/round.rb
@@ -18,4 +18,23 @@ class Round < ActiveRecord::Base
 
   belongs_to :tournament
   has_many :matches
+
+  def get_overall
+    sql = "SELECT users.id, users.username,
+        sum(mp.score) as summscore,
+        users.slug
+      FROM match_predicts mp
+      JOIN users on user_id = users.id
+      JOIN matches on match_id = matches.id
+      JOIN rounds on rounds.id = matches.round_id
+      WHERE rounds.id = 1
+      GROUP by mp.user_id
+      ORDER by summscore desc"
+
+    result = Array.new
+    ActiveRecord::Base.connection.execute(sql).each do |row|
+      result.push [row[0], row[1], row[2], row[4]]
+    end
+    return result
+  end
 end
